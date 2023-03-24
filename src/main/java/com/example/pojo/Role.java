189ec775411edb034600b044b28c92fac62522f8
hehepeng <18846042826@163.com>
2023-03-24 10:34:01 +0800

    更改仓库

---
+++ src/main/java/com/example/pojo/Role.java
@@ -0,0 +1,35 @@
+package com.example.pojo;
+
+import java.util.List;
+
+public class Role {
+    // 角色ID
+    private int id;
+
+    // 角色名称
+    private String roleName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id='" + id + '\'' +
+                ", roleName='" + roleName + '\'' +
+                '}';
+    }
+}
